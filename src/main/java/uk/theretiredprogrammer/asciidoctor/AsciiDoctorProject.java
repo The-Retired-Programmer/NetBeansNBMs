b89a398efe4e2a1a566695135b44d7aaa9b8887a
Richard Linsdale <richard@theretiredprogrammer.uk>
2022-06-26 23:26:48 +0100

    Basic functionality (book building and web page building) added.

---
+++ src/main/java/uk/theretiredprogrammer/asciidoctor/AsciiDoctorProject.java
@@ -0,0 +1,195 @@
+/*
+ * Copyright 2018-2022 richard linsdale.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.theretiredprogrammer.asciidoctor;
+
+import java.awt.Image;
+import java.beans.PropertyChangeListener;
+import javax.swing.Action;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import org.netbeans.api.annotations.common.StaticResource;
+import org.netbeans.api.project.Project;
+import org.netbeans.api.project.ProjectInformation;
+import org.netbeans.spi.project.ProjectState;
+import org.netbeans.spi.project.ui.LogicalViewProvider;
+import org.netbeans.spi.project.ui.support.CommonProjectActions;
+import org.openide.filesystems.FileObject;
+import org.openide.loaders.DataFolder;
+import org.openide.loaders.DataObjectNotFoundException;
+import org.openide.nodes.AbstractNode;
+import org.openide.nodes.Children;
+import org.openide.nodes.FilterNode;
+import org.openide.nodes.Node;
+import org.openide.util.ImageUtilities;
+import org.openide.util.Lookup;
+import org.openide.util.lookup.Lookups;
+import org.openide.util.lookup.ProxyLookup;
+import uk.theretiredprogrammer.actionssupport.DynamicAction;
+
+public class AsciiDoctorProject implements Project {
+
+    private final FileObject projectDir;
+    private final ProjectState state;
+    private Lookup lkp;
+
+    /**
+     * Constructor
+     * 
+     * @param dir project root folder
+     * @param state the project state
+     */
+    AsciiDoctorProject(FileObject dir, ProjectState state) {
+        this.projectDir = dir;
+        this.state = state;
+    }
+
+    @Override
+    public FileObject getProjectDirectory() {
+        return projectDir;
+    }
+
+    @Override
+    public Lookup getLookup() {
+        if (lkp == null) {
+            lkp = Lookups.fixed(new Object[]{
+                new Info(),
+                new LogicalView(this)
+            });
+        }
+        return lkp;
+    }
+
+    private final class Info implements ProjectInformation {
+
+        @StaticResource()
+        public static final String ICON = "uk/theretiredprogrammer/asciidoctor/folder_edit.png";
+
+        @Override
+        public Icon getIcon() {
+            return new ImageIcon(ImageUtilities.loadImage(ICON));
+        }
+
+        @Override
+        public String getName() {
+            return getProjectDirectory().getName();
+        }
+
+        @Override
+        public String getDisplayName() {
+            return getName();
+        }
+
+        @Override
+        public void addPropertyChangeListener(PropertyChangeListener pcl) {
+            //do nothing, won't change
+        }
+
+        @Override
+        public void removePropertyChangeListener(PropertyChangeListener pcl) {
+            //do nothing, won't change
+        }
+
+        @Override
+        public Project getProject() {
+            return AsciiDoctorProject.this;
+        }
+    }
+
+    class LogicalView implements LogicalViewProvider {
+
+        @StaticResource()
+        public static final String ICON = "uk/theretiredprogrammer/asciidoctor/folder_edit.png";
+
+        private final AsciiDoctorProject project;
+
+        public LogicalView(AsciiDoctorProject project) {
+            this.project = project;
+        }
+
+        @Override
+        public Node createLogicalView() {
+            try {
+                //Obtain the project directory's node:
+                FileObject projectDirectory = project.getProjectDirectory();
+                DataFolder projectFolder = DataFolder.findFolder(projectDirectory);
+                Node nodeOfProjectFolder = projectFolder.getNodeDelegate();
+                //Decorate the project directory's node:
+                return new ProjectNode(nodeOfProjectFolder, project);
+            } catch (DataObjectNotFoundException donfe) {
+                //Fallback-the directory couldn't be created -
+                //read-only filesystem or something evil happened
+                return new AbstractNode(Children.LEAF);
+            }
+        }
+
+        private final class ProjectNode extends FilterNode {
+
+            final AsciiDoctorProject project;
+
+            public ProjectNode(Node node, AsciiDoctorProject project)
+                    throws DataObjectNotFoundException {
+                super(node,
+                        //NodeFactorySupport.createCompositeChildren(
+                        //        project,
+                        //        "Projects/uk-theretiredprogrammer-jbake/Nodes"),
+                        new FilterNode.Children(node),
+                        new ProxyLookup(
+                                new Lookup[]{
+                                    Lookups.singleton(project),
+                                    node.getLookup()
+                                }));
+                this.project = project;
+            }
+
+            @Override
+            public Action[] getActions(boolean arg0) {
+                return new Action[]{
+                    CommonProjectActions.renameProjectAction(),
+                    CommonProjectActions.copyProjectAction(),
+                    CommonProjectActions.closeProjectAction(),
+                    null,
+                    new DynamicAction(projectDir, testexists(projectDir, "assemblebook", "adoc", "Build book"), "bash -c \"cd ${PATH} && asciidoctor-pdf -d book -a toc -o target/book.pdf assemblebook.adoc\""),
+                    new DynamicAction(projectDir, testexists(projectDir, "assemble", "webresources", testexists(projectDir, "assemblewebpage", "adoc", "Build webpage")), "bash -c \"cd ${PATH} && asciidoctor -d article -a toc2 -o target/webpage.html assemblewebpage.adoc && ./assemble.webresources\"")
+                };
+            }
+            
+            private String testexists(FileObject directory, String filename, String ext, String labelifexists) {
+                return directory.getFileObject(filename,ext)!= null ? labelifexists: null;
+            }
+
+            @Override
+            public Image getIcon(int type) {
+                return ImageUtilities.loadImage(ICON);
+            }
+
+            @Override
+            public Image getOpenedIcon(int type) {
+                return getIcon(type);
+            }
+
+            @Override
+            public String getDisplayName() {
+                return project.getProjectDirectory().getName();
+            }
+        }
+
+        @Override
+        public Node findPath(Node root, Object target) {
+            //leave unimplemented for now
+            return null;
+        }
+    }
+}
