64a4d6a01f8dfff1e974d7b7312dad96594af302
Richard Linsdale <richard@theretiredprogrammer.uk>
2023-12-11 22:02:38 +0000

    Minor changes to testing - due to changes to the system.rules to a set for basic functionality.

--- html2textile/src/test/java/uk/theretiredprogrammer/html2textile/rules/Rules_Test.java
+++ html2textile/src/test/java/uk/theretiredprogrammer/html2textile/rules/Rules_Test.java
@@ -25,100 +25,42 @@ public class Rules_Test {
 
     public static final String SANDBOX_INPUT = "/home/richard/SANDBOX/SANDBOX/dinghies.html";
     public static final String SANDBOX_INPUT2 = "/home/richard/SANDBOX/SANDBOX2/dinghies.html";
+    public static final String SANDBOX_INPUT2A = "/home/richard/SANDBOX/SANDBOX2/dinghiesCopy.html";
 
     @Test
     public void test1() throws IOException {
         Rules.create();
         run_HTML_PREPROCESSING_test(
                 "abc|;|&nbsp;|&lsquo;|&rsquo;|&|xyz",
-                "abc|;| |'|'|&|xyz"
+                "abc|;|&nbsp;|&lsquo;|&rsquo;|&|xyz"
         );
     }
-
+    
     @Test
     public void test2() throws IOException {
         Rules.create(new File(SANDBOX_INPUT));
         run_HTML_PREPROCESSING_test(
                 "abc|;|&nbsp;|&lsquo;|&rsquo;|&|xyz",
-                "abc|;| |'|!!!|&|xyz"
+                "abc|;| |'|'|&|xyz"
         );
     }
-
+    
     @Test
-    public void test5() throws IOException {
-        Rules.create();
-        run_TEXTILE_POSTPROCESSING_test(
-                "h3{text-align:center;color:red;}. text-text-text\n",
-                "h3{color:red;}=. text-text-text\n"
-        );
-    }
-
-    @Test
-    public void test6() throws IOException {
-        Rules.create();
-        run_TEXTILE_POSTPROCESSING_test(
-                "p{margin:020px10px0px;float:left;width:50%;}. text-text-text\n",
-                "p(float-left-50). text-text-text\n"
-        );
-    }
-
-    @Test
-    public void test7() throws IOException {
-        Rules.create(new File(SANDBOX_INPUT));
-        run_TEXTILE_POSTPROCESSING_test(
-                "h3{text-align:center;color:red;}. text-text-text\n",
-                "h3{color:red;}=. text-text-text\n"
-        );
-    }
-
-    @Test
-    public void test8() throws IOException {
-        Rules.create(new File(SANDBOX_INPUT));
-        run_TEXTILE_POSTPROCESSING_test(
-                "p{margin:020px10px0px;float:left;width:50%;}. text-text-text\n",
-                "p(float-PORT-50). text-text-text\n"
-        );
-    }
-
-    @Test
-    public void test9() throws IOException {
+    public void test3() throws IOException {
         Rules.create(new File(SANDBOX_INPUT2));
         run_HTML_PREPROCESSING_test(
                 "abc|;|&nbsp;|&lsquo;|&rsquo;|&|xyz",
-                "abc|;|&nbsp;|&lsquo;|!!!|&|xyz"
+                "abc|;| |'|!!!|&|xyz"
         );
     }
-
+    
     @Test
-    public void test10() throws IOException {
-        Rules.create(new File(SANDBOX_INPUT2));
-        run_TEXTILE_POSTPROCESSING_test(
-                "h3{text-align:left;color:red;}. text-text-text\n",
-                "h3{text-align:left;color:red;}. text-text-text\n"
+    public void test4() throws IOException {
+        Rules.create(new File(SANDBOX_INPUT2A));
+        run_HTML_PREPROCESSING_test(
+                "abc|;|&nbsp;|&lsquo;|&rsquo;|&|xyz",
+                "abc|;| |{}|???|&|xyz"
         );
-    }
-
-    @Test
-    public void test11() throws IOException {
-        Rules.create(new File(SANDBOX_INPUT));
-        run_TEXTILE_POSTPROCESSING_test(
-                "p{width:100%;margin:0;padding:0;}. text-text-text\n",
-                "text-text-text\n"
-        );
-    }
-
-    @Test
-    public void test12() throws IOException {
-        Rules.create(new File(SANDBOX_INPUT2));
-        run_TEXTILE_POSTPROCESSING_test(
-                "p{width:100%;margin:0;padding:0;}. text-text-text\n",
-                "p{width:100%;margin:0;padding:0;}. text-text-text\n"
-        );
-    }
-
-    private void run_TEXTILE_POSTPROCESSING_test(String input, String expected) throws IOException {
-        StringProxy s = Rules.get_TEXTILE_POSTPROCESSING();
-        assertEquals(expected, s.applyRules(input));
     }
 
     private void run_HTML_PREPROCESSING_test(String input, String expected) throws IOException {
